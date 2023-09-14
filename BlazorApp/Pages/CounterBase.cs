1c1ecc5d6ed47a392f964db3ac9ef8f984fde078
Mahdi Pakzad <elias-developer@outlook.com>
2023-09-14 18:22:52 +0330

    Add project files.

---
+++ BlazorApp/Pages/CounterBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Pages;
+
+public class CounterBase:ComponentBase
+{
+    [Inject]
+    public ILogger<CounterBase> Logger { get; set; }
+
+    protected int currentCount = 0;
+
+    protected void IncrementCount()
+    {
+        currentCount++;
+        Logger.LogInformation("Hello {counterValue}",currentCount);
+    }
+}
---
+++ BlazorApp/Pages/FetchData.cs
@@ -0,0 +1,20 @@
+﻿using BlazorApp.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Pages;
+
+public partial class FetchData
+{
+
+
+    private WeatherForecast[]? forecasts;
+
+    [Inject]
+    public WeatherForecastService ForecastService { get; set; } 
+    protected override async Task OnInitializedAsync()
+    {
+       await Task.Delay(5000);
+        forecasts = await ForecastService.GetForecastAsync(DateOnly.FromDateTime(DateTime.Now));
+    }
+
+}
